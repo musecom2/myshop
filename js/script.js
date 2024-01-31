c542dce6662daa58b159901e159e6726e019ab14
musecom2 <musecom2@gmail.com>
2024-01-31 12:17:28 +0900

    메뉴 json 구성

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+    $(document)
+    .on("mouseenter", '.pr-category>li', function(){
+        $(this).find('.sub-cate').fadeIn();
+    })
+    .on("mouseleave", '.pr-category>li', function(){
+        $(".sub-cate").fadeOut();
+    });
+    
+//    $('.pr-category>li').hover(function(){
+//       $(this).find('.sub-cate').fadeToggle();
+//    });
+});
