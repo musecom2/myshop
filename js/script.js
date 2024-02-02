80470749d9942e88bfd2d936df627764c2378cd0
musecom2 <musecom2@gmail.com>
2024-02-02 14:41:32 +0900

    list 보기 jquery 등록

--- js/script.js
+++ js/script.js
@@ -14,6 +14,21 @@ $(function(){
        $(this).find('.category-subnav').css('display', 'none');
     });
 
-
+   $('.listview').click(function(e){
+       e.preventDefault();
+       const view = $(this).data("view");
+       $("#pdlist>div").removeClass();
+       $('.listview rect').removeClass('list-act-color').addClass('list-color');
+       if(view == 3){
+          $(this).find('rect').removeClass('list-color').addClass('list-act-color');
+          $("#pdlist>div").addClass("col-md-4 mb-5");
+       }else if(view == 4){
+          $(this).find('rect').removeClass('list-color').addClass('list-act-color');
+          $("#pdlist>div").addClass("col-md-3 mb-5");
+       }else if(view == 5){
+          $(this).find('rect').removeClass('list-color').addClass('list-act-color');
+          $("#pdlist>div").addClass("col-md-55 mb-5");
+       }
+   })
     
 });
